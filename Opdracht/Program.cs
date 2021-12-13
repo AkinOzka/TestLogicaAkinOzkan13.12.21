a9f30d341e15bfcae00036c69f0ed831cb9dcf2b
AkinO <AkinO@PC215-08>
2021-12-13 13:32:27 +0100

    Add project files.

---
+++ Opdracht/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opdracht
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Left = new int[] {1,3,2,6,9};
+            int[] Right = new int[] { 0, 0, 0, 0, 0 };
+
+            for (int i = 0; i < Left.Length; i++)
+            {
+                Console.WriteLine(Right.CopyTo(Left[]));
+            }
+        }
+    }
+}
